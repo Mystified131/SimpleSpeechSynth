f6ca8c06fae66c71c7a3bc24f0901d9824963583
Thomas Jackson Park <42450501+Mystified131@users.noreply.github.com>
2020-10-31 21:37:34 -0500

    Add files via upload

---
+++ SpeechSynth.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+import os    
+
+speechstr = "Happy Halloween, Torrey"
+tts = gTTS(text=speechstr, lang='en')
+tts.save("pcvoice.mp3")
+# to start the file from python
+os.system("start pcvoice.mp3")
